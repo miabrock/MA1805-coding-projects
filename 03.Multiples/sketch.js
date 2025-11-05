69d0d17df94284961629926d78c9b62956d9887c
miabrock <mia.j.brock@icloud.com>
2025-11-05 17:23:50 +0000

    Add files via upload

--- 03.Multiples/sketch.js
+++ 03.Multiples/sketch.js
@@ -1,7 +1,25 @@
+let theText = ["Some", "Text", "To", "Display"]
+let word = theText[0];
+let i = 0;
+
 function setup() {
   createCanvas(400, 400);
+  frameRate(5);
 }
 
 function draw() {
-  background(220);
+  background(200);
+
+  // The text display
+  textSize(40);
+  text(word, 20, height/2)
+
+  // change the value of word
+  word = theText[i];
+  i=i+1;
+  if(i>theText.length){
+    i=0;
+  }
+  console.log(i)
 }
+
