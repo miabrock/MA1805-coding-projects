01339d2863c9fe735d28233f130c6c72e744c1a7
miabrock <mia.j.brock@icloud.com>
2025-11-05 02:28:31 +0000

    Add files via upload

--- 02.Movement/sketch.js
+++ 02.Movement/sketch.js
@@ -1,11 +1,21 @@
+ let i = 0;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
-  fill(255, 0, 0);
-  stroke(0, 255, 0);
-  strokeWeight(10); 
-  circle(100,150,200)
+
+  // eyes
+  fill(100, 100, 0);
+  circle(50, i, 50, 50);
+  circle(200, i, 50, 50);
+
+  // mouth
+  fill(255, 25, 0, 40);
+  rect(i, 150, i, 20);
+
+  // increment i
+  i = i+1;
 }
