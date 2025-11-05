7de7583af1034f7cf1b7a6df293979cbf3304425
miabrock <mia.j.brock@icloud.com>
2025-11-05 17:52:05 +0000

    Add files via upload

--- 04.media/sketch.js
+++ 04.media/sketch.js
@@ -1,8 +1,15 @@
+let img;
+
+function preload() {
+  img = loadImage('leonid-privalov-eXuWi5jvXN0-unsplash (1).jpg');
+}
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
-  circle(50, 50, 50)
+  // draw the image.
+  image(img, 0, 0);
 }
